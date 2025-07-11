ed60bba019f70118e110564b742fd119dedf547d
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-07-11 04:54:44 +0100

    Discord Embed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,12 @@
 
 <svelte:head>
     <title>Stardew Festivals</title> 
+    <meta content="Stardew Festival Positions" property="og:title" />
+    <meta content="https://festivals.pillow.rocks/favicon.png" property="og:image" />
+    <meta content="An interactive online viewer to see festival positions of Stardew Valley NPCs (modded and vanilla)" property="og:description" />
+    <meta content="https://festivals.pillow.rocks" property="og:url" />
+    <meta content="#FF42A6" name="theme-color" />
+    <meta property="og:type" content="website" />
 </svelte:head>
 
 <ModeWatcher />
