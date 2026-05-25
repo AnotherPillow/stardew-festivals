c0896f1a40467e5d1dc04856e40c9cdc5f530552
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-05-25 15:47:35 +0100

    add umami

---
+++ scripts/generate-compiled-files.py
@@ -0,0 +1,40 @@
+NAME_COMPARISON = [
+    {
+        "filename": "eggfestival/main-sve-2.ts",
+    },
+    {
+        "filename": "eggfestival/main-sve.ts",
+    },
+    {
+        "filename": "eggfestival/main-vanilla-2.ts",
+    },
+    {
+        "filename": "eggfestival/main-vanilla.ts",
+    },
+    {
+        "filename": "eggfestival/setup-sve-2.ts",
+    },
+    {
+        "filename": "eggfestival/setup-sve.ts",
+    },
+    {
+        "filename": "eggfestival/setup-vanilla-2.ts",
+    },
+    {
+        "filename": "eggfestival/setup-vanilla.ts",
+    },
+    
+    
+    {
+        "filename": "fair/vanilla-1.ts",
+    },
+    {
+        "filename": "fair/vanilla-2.ts",
+    },
+    {
+        "filename": "fair/sve-1.ts",
+    },
+    {
+        "filename": "fair/sve-2.ts",
+    },
+]
